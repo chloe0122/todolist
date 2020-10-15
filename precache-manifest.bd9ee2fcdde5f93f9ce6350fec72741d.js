5f525d6a8c09ec17f47c746f409c43380adafddc
chloe0122 <kyung0446@gmail.com>
2020-10-15 10:59:05 +0900

    Updates

---
+++ precache-manifest.bd9ee2fcdde5f93f9ce6350fec72741d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20cb92222bad54d3e9f55378f2ff1b10",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "80f12f5c42a894b30809",
+    "url": "/todolist/static/css/main.2450898d.chunk.css"
+  },
+  {
+    "revision": "70d24e14d0e51be8050e",
+    "url": "/todolist/static/js/2.3d1b521d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todolist/static/js/2.3d1b521d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f12f5c42a894b30809",
+    "url": "/todolist/static/js/main.44900e16.chunk.js"
+  },
+  {
+    "revision": "be1ea64cf07e1e2b9473",
+    "url": "/todolist/static/js/runtime-main.992438bd.js"
+  }
+]);
